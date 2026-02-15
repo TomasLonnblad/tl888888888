9ae6ca4e193f7cb123778d0a3eb356f553243b68
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-15 14:31:06 +0100

    x

---
+++ src/app/(main)/dashboard/carousel2/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+ ["images.remotePatterns", "https://tomaslonnblad.github.io/", "https://assets.vercel.com/image/upload/v1538361091/repositories/next-js/next-js-bg.png" , 
+// images.unsplash.com
+];
+const nextConfig = {
+
+images:{domains: ["images.remotePatterns", "https://tomaslonnblad.github.io/", "https://assets.vercel.com/image/upload/v1538361091/repositories/next-js/next-js-bg.png"
+// images.unsplash.com
+]},
+  reactCompiler: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/default",
+        permanent: false,
+      },
+    ];
+  },
+};
+export default nextConfig;
---
+++ src/app/(main)/dashboard/motion4/page.tsx
@@ -0,0 +1,19 @@
+
+
+import PageNavbar, { PageNavbarIconButton, PageNavbarLeftContent, PageNavbarPrimaryButton, PageNavbarRightContent } from '@/components/layout/PageNavbar'
+
+function Motion4() {
+    return (
+        <div>
+            <>
+                <PageNavbarLeftContent>
+                    {/* <div className='flex items-center'> */}
+                           
+Hå o hej
+                    {/* </div> */}
+                </PageNavbarLeftContent>
+ </>
+        </div>
+    )
+}
+export default Motion4
